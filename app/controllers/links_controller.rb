63f1ed680284bdd7f21e4e55620f0bb23ed8fd4d
Jay Wengrow <jaywngrw@gmail.com>
2014-11-02 11:59:46 -0600

    added link validations and stripped http

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -10,12 +10,22 @@ class LinksController < ApplicationController
 
   def create
     @link = current_user.links.new(params[:link])
+    @link.strip_http!
 
     if @link.save
       flash[:success] = "Link created successfully."
       redirect_to links_path
     else
       render 'new'
+    end
+  end
+
+  def redirect
+    @link = Link.find_by(:slug => params[:slug])
+    if @link
+      redirect_to "http://#{@link.target_url}"
+    else
+      raise ActionController::RoutingError.new('Not Found')
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get 'links/new' => 'links#new', as: :new_link
   post 'links' => 'links#create'
 
+  get '/:slug' => 'links#redirect' 
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
